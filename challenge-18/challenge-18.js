30fb679e36ca14264d330bab5bffc98b65608f9e
alpaula <anna.luiza.araujo@hotmail.com>
2019-05-15 20:47:49 -0300

    Conclusão desafio semana 18

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -1,4 +1,6 @@
-(function () {/*
+(function () {
+	'use strict';
+	/*
 	1. Envolva todo o conteúdo desse desafio em uma IIFE.
 	2. Adicione a diretiva 'use strict';
 	3. Crie um arquivo index.html e adicione esse script à ele.
